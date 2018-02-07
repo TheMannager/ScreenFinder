4cbb818e1e7b7bb31d249f13dce364b250695e7a
TheMannager <nathanielcmann@gmail.com>
2018-02-07 13:10:45 -0600

    cleaned things up quite a bit.

--- helperFunct.py
+++ helperFunct.py
@@ -1,38 +1,7 @@
-#Variable decleration
-
-NUM        = 0
-DENOM      = 1
-
-
-def gcd(a, b):
-    """Calculate the Greatest Common Divisor of a and b.
-
-        Unless b==0, the result will have the same sign as b (so that when
-        b is divided by it, the result comes out positive).
-        """
-    while b:
-        a, b = b, a % b
-    return a
-
-#(x,y)
-def simplify(numer, denom, which):
-
-    if denom == 0:
-        return "Division by 0 - result undefined"
-
-    # Remove greatest common divisor:
-    common_divisor = gcd(numer, denom)
-    (reduced_num, reduced_den) = (numer / common_divisor, denom / common_divisor)
-    # Note that reduced_den > 0 as documented in the gcd function.
-    if which == 0:
-        return reduced_num
-    if which == 1:
-        return reduced_den
-
+#height = diagonal / sqrt(1 + ratio^2)
 def find_height(size, vert, hor):
-    ratio   = ((simplify(hor, vert, NUM)) / (simplify(hor, vert, DENOM)))
-    return (size/((1 + ratio**2 )**(1/2)))
+    return size / ((1 + (hor / float(vert))**(2))**(1/float(2)))
+
 
 def find_width(size, vert, hor):
-    ratio   = ((simplify(hor, vert, NUM)) / (simplify(hor, vert, DENOM)))
-    return find_height(size, vert, hor) * ratio
+    return find_height(size, vert, hor) * (hor / float(vert))
--- screenFinder.py
+++ screenFinder.py
@@ -1,12 +1,11 @@
-
-
-
-
 from helperFunct import *
 
 
 #main area
 monitor_count = input('Enter number of monitors: ')
 for x in range(1,monitor_count+1):
-    print( round(find_height(input("input size of monitor %d: " % x),1080,1920), 2))
+    #ratio   = ( float(1920) / float(1080))
+    #print ratio
+    #print( float(27) / ((1 + ratio*ratio )**(1/2)))
+    print( (find_height(input("input size of monitor %d: " % x),1080,1920)))
 #print( round(find_height(27,1080,1920), 2))
