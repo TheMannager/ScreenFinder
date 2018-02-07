e69b04792ec484cea7b7489fbd72e5f417c17aa5
TheMannager <nathanielcmann@gmail.com>
2018-02-07 13:13:39 -0600

    done for today. Find height and find width are both workig now.

--- screenFinder.py
+++ screenFinder.py
@@ -4,8 +4,5 @@ from helperFunct import *
 #main area
 monitor_count = input('Enter number of monitors: ')
 for x in range(1,monitor_count+1):
-    #ratio   = ( float(1920) / float(1080))
-    #print ratio
-    #print( float(27) / ((1 + ratio*ratio )**(1/2)))
-    print( (find_height(input("input size of monitor %d: " % x),1080,1920)))
+    print( (find_height(input("input size of monitor %d: " % x),input("input vertical resolution of monitor %d: " % x),input("input horizontal resolution of monitor %d: " % x))))
 #print( round(find_height(27,1080,1920), 2))
